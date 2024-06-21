c9c93083f4645127775716e71a46d56189ea6f3b
xratq <44581405+xratq@users.noreply.github.com>
2024-06-21 22:07:39 +0300

    Update index.php

--- rew/index.php
+++ rew/index.php
@@ -31,7 +31,7 @@ $APPLICATION->SetTitle("rew");
 		"DETAIL_PROPERTY_CODE" => array(
 			0 => "POSITION",
 			1 => "COMPANY",
-			2 => "",
+			2 => "FILE",
 		),
 		"DETAIL_SET_CANONICAL_URL" => "N",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
