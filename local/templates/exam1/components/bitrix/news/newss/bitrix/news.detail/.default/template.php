ff5025cd1e06953f26596b0bccfc6e0c83b7f3cc
xratq <44581405+xratq@users.noreply.github.com>
2024-06-29 11:50:35 +0300

    Add folder "local"

---
+++ local/templates/exam1/components/bitrix/news/newss/bitrix/news.detail/.default/template.php
@@ -0,0 +1,69 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$this->setFrameMode(true);
+
+?>
+
+<div class="review-block">
+    <div class="review-text">
+        <div class="review-text-cont">
+        <?php if (!empty($arResult["DETAIL_TEXT"]) && is_string($arResult["DETAIL_TEXT"])): ?>
+                <?= $arResult["DETAIL_TEXT"] ?>
+            <?php endif; ?>
+        </div>
+        <div class="review-autor">
+            <?php if (!empty($arResult["NAME"]) && is_string($arResult["NAME"])): ?>
+                <?= $arResult["NAME"] ?>
+            <?php endif; ?>
+            
+            <?php if (!empty($arResult["DISPLAY_ACTIVE_FROM"]) && is_string($arResult["DISPLAY_ACTIVE_FROM"])): ?>
+                <?= $arResult["DISPLAY_ACTIVE_FROM"] ?>
+            <?php endif; ?>
+            
+            <?php if (!empty($arResult["DISPLAY_PROPERTIES"]["POSITION"]["VALUE"]) && is_string($arResult["DISPLAY_PROPERTIES"]["POSITION"]["VALUE"])): ?>
+                <?= $arResult["DISPLAY_PROPERTIES"]["POSITION"]["VALUE"] ?>
+            <?php endif; ?>
+            <?php if (!empty($arResult["DISPLAY_PROPERTIES"]["COMPANY"]["VALUE"]) && is_string($arResult["DISPLAY_PROPERTIES"]["COMPANY"]["VALUE"])): ?>
+                <?= $arResult["DISPLAY_PROPERTIES"]["COMPANY"]["VALUE"] ?>
+            <?php endif; ?>
+            
+        </div>
+    </div>
+
+
+
+    <div style="clear: both;" class="review-img-wrap">
+        <?
+      if (!empty($arItem["PREVIEW_PICTURE"]["SRC"])) {
+        ?>
+<img src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>" alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>">
+<?  }
+else  { ?>
+
+    <img src="<?=SITE_TEMPLATE_PATH?>/img/no_photo.jpg" alt="img">
+
+<? } ?>
+    </div>
+</div>
+
+
+
+<?php if (!is_null($arResult["DISPLAY_PROPERTIES"]["FILE"]["VALUE"]) && is_array($arResult["DISPLAY_PROPERTIES"]["FILE"]["VALUE"])):  ?>
+
+    <div class="exam-review-doc">
+        <p><?= GetMessage("TITLE"); ?>:</p>
+        
+        <?php 
+        $fileValues = $arResult["DISPLAY_PROPERTIES"]["FILE"]["FILE_VALUE"];
+        if (!is_array($fileValues) || isset($fileValues["SRC"])) {
+            $fileValues = array($fileValues);
+        }
+        foreach ($fileValues as $val): ?>
+            <div class="exam-review-item-doc">
+                <img class="rew-doc-ico" src="<?=SITE_TEMPLATE_PATH?>/img/icons/pdf_ico_40.png">
+                <a href="<?= $val["SRC"] ?>"><?= $val["ORIGINAL_NAME"] ?></a>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+<?php endif; ?>
---
+++ local/templates/exam1/components/bitrix/news/newss/bitrix/news.list/.default/template.php
@@ -0,0 +1,77 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="news-list">
+
+<?php
+if (empty($arResult["ITEMS"])) {
+    return;
+}
+?>
+<?foreach($arResult["ITEMS"] as $arItem):?>
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+	<div class="review-block" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+		<div class="review-text">
+							
+			<div class="review-block-title">
+				<span class="review-block-name">
+					<a href=<?echo $arItem["DETAIL_PAGE_URL"]?>>
+						<?=$arItem ["NAME"];?>
+					</a>
+				</span>
+				<span class="review-block-description">
+					<?echo $arItem["DISPLAY_ACTIVE_FROM"]?>
+					<?php
+        if (!empty($arItem["DISPLAY_PROPERTIES"]["POSITION"]["VALUE"]) && is_string($arItem["DISPLAY_PROPERTIES"]["POSITION"]["VALUE"])) {
+			echo $arItem["DISPLAY_PROPERTIES"]["POSITION"]["VALUE"];
+		}
+		if (!empty($arItem["DISPLAY_PROPERTIES"]["COMPANY"]["VALUE"]) && is_string($arItem["DISPLAY_PROPERTIES"]["COMPANY"]["VALUE"])) {
+			echo $arItem["DISPLAY_PROPERTIES"]["COMPANY"]["VALUE"];
+		}
+		?>
+					
+				</span>
+			</div>
+			
+			<div class="review-text-cont">
+			<?php
+        if (!empty($arItem["PREVIEW_TEXT"]) && is_string($arItem["PREVIEW_TEXT"])) {
+			echo $arItem["PREVIEW_TEXT"];
+		}
+		?>
+				
+			</div>
+		</div>
+		<div class="review-img-wrap">
+			<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>">
+			
+			<?
+            if (!empty($arItem["PREVIEW_PICTURE"]["SRC"])) {
+            ?>
+            <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>">
+            <?  }
+            else  { ?>
+
+                <img src="<?=SITE_TEMPLATE_PATH?>/img/no_photo.jpg" alt="img">
+
+            <? } ?>
+
+        </a>
+	</div>
+	</div>
+	<?endforeach;?>
+<?=$arResult["NAV_STRING"]?>
